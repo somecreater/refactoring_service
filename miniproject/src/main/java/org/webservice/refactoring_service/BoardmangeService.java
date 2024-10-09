91b5bfeb3e0c083d2cfd3f8aae2f299c4e4406b9
somecreater <dbstjrgy123@gmail.com>
2024-10-09 14:57:42 +0900

    서비스 인터페이스 클래스 추가

---
+++ miniproject/src/main/java/org/webservice/refactoring_service/BoardmangeService.java
@@ -0,0 +1,4 @@
+package org.webservice.refactoring_service;
+
+public interface BoardmangeService {
+}
---
+++ miniproject/src/main/java/org/webservice/task/ScheduledService.java
@@ -0,0 +1,10 @@
+package org.webservice.task;
+
+public class ScheduledService {
+
+    //하루에 한 번씩 서버 내 파일과 db상의 파일 정보 비교하여 동기화
+
+    //하루에 한 번씩 Redis 내 남은 데이터 삭제
+
+
+}
