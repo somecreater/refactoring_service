b32f36f7f7858fede516613ab79809f15d89904a
somecreater <dbstjrgy123@gmail.com>
2024-10-10 23:49:29 +0900

    redis 테스트 코드 추가

--- miniproject/src/test/java/org/webservice/persis/RedisTests.java
+++ miniproject/src/test/java/org/webservice/persis/RedisTests.java
@@ -3,18 +3,28 @@ package org.webservice.persis;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.webservice.entity.BoardCacheEntity;
+import org.webservice.redis_repository.BoardCacheRepository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class RedisTests {
 
+    /*
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
     @Autowired
     private RedisTemplate<String, String> redisTemplate;
+    */
+    @Autowired
+    private BoardCacheRepository boardCacheRepository;
 
-    @Test
+    //@Test
+    /*
     public void testRedisConnection() {
         ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
 
@@ -26,4 +36,16 @@ public class RedisTests {
 
         assertThat(storedValue).isEqualTo(value);
     }
+    */
+
+    @Test
+    public void testRedisRepository(){
+        BoardCacheEntity boardCache=BoardCacheEntity.builder().bno(1L).viscount(12).recommendation(12).build();
+        boardCacheRepository.save(boardCache);
+
+        BoardCacheEntity boardCache1=boardCacheRepository.findById(1L).get();
+        assertThat(boardCache1.getViscount()).isEqualTo(boardCache.getViscount());
+        assertThat(boardCache1.getRecommendation()).isEqualTo(boardCache.getRecommendation());
+    }
+
 }
