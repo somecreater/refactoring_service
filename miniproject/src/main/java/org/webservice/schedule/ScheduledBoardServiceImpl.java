78247c7450aafb8eea283fa3d9c19da22452cdaf
somecreater <dbstjrgy123@gmail.com>
2024-10-30 14:45:27 +0900

    주기적인 게시글 추천수, 조회수 업데이트 서비스 구현

--- miniproject/src/main/java/org/webservice/schedule/ScheduledBoardServiceImpl.java
+++ miniproject/src/main/java/org/webservice/schedule/ScheduledBoardServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.transaction.annotation.Transactional;
 import org.webservice.entity.BoardCacheEntity;
 import org.webservice.entity.BoardEntity;
 import org.webservice.redis_repository.BoardCacheRepository;
@@ -24,28 +25,43 @@ public class ScheduledBoardServiceImpl implements ScheduledBoardService{
 
     @Override
     @Scheduled(fixedRate = 10000)
+    @Transactional
     public void BoardCacheUpdate(){
 
         Iterable<BoardCacheEntity> cachelist=boardCacheRepository.findAll();
-        List<BoardEntity> boardEntities=new ArrayList<>();
+        List<Long> bnolist = new ArrayList<>();
+        for (BoardCacheEntity cache : cachelist) {
+            bnolist.add(cache.getBno());
+        }
+
+        List<BoardEntity> boardEntities=boardRepository.findAllById(bnolist);
+        Map<Long, BoardEntity> boardEntityMap=new HashMap<>();
+        for(BoardEntity boardEntity:boardEntities){
+           boardEntityMap.put(boardEntity.getBno(),boardEntity);
+        }
 
         redisTemplate.executePipelined((RedisCallback<Object>)connection ->{
 
             for(BoardCacheEntity boardCacheEntity:cachelist){
                 Long bno=boardCacheEntity.getBno();
-                BoardEntity boardEntity=boardRepository.findById(bno).orElse(null);
                 int recommendation=boardCacheEntity.getRecommendation();
                 long viscount=boardCacheEntity.getViscount();
+
+                BoardEntity boardEntity=boardEntityMap.get(bno);
                 if(boardEntity!=null){
                     boardEntity.setViscount(boardEntity.getViscount()+viscount);
                     boardEntity.setRecommendation(boardEntity.getRecommendation()+recommendation);
-                    //추후구현
-
+                    boardEntityMap.replace(bno,boardEntity);
                 }
             }
 
+            boardCacheRepository.deleteAll(cachelist);
             return null;
         });
+
+        if(!boardEntityMap.isEmpty()){
+            boardRepository.saveAll(boardEntityMap.values());
+        }
     }
 
 }
